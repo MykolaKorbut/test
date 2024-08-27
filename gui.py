1a861a187ebaeefc0052b435e023dd779d825569
mikolakorbut <nikolay.korbut@gmail.com>
2024-08-27 19:45:54 +0300

    web app creating start

--- gui.py
+++ gui.py
@@ -12,12 +12,16 @@ sg.theme("Black")
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button(size=2, image_source="add.png", mouseover_colors="LightBlue2",
+add_button = sg.Button(size=2,
+                       image_source="/Users/mikolakorbut/PycharmProjects/pythonProject/add.png",
+                       mouseover_colors="LightBlue2",
                        tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
-complete_button = sg.Button(size=2, image_source="complete.png", mouseover_colors="LightBlue2",
+complete_button = sg.Button(size=2,
+                            image_source="/Users/mikolakorbut/PycharmProjects/pythonProject/complete.png",
+                            mouseover_colors="LightBlue2",
                             tooltip="Complete", key='Complete')
 exit_button = sg.Button("Exit")
 
